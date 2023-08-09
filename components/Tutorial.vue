5d1e4eb1da740fb12e47a770da6d2e709e2758f1
flovis <floviskabandafkm@gmail.com>
2023-08-09 10:02:06 +0200

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,26 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="h-screen flex justify-center items-center w-md">
+    <form>
+      <p class="text-center mb-8 font-bold text-lg">
+        Renilialiser votre mot de passe
+      </p>
+      <input
+        type="text"
+        name=""
+        id=""
+        class="p-3 w-full border border-gray rounded-md text-md"
+        placeholder="Entrer votre agres mail"
+      />
+      <button class="w-full p-3 text-white bg-black rounded-md mt-4 font-bold">
+        Envoyer
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
